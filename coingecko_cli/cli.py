9c6650a235d65fe1aa48e23b515cf12bfa63bc18
0x4nz0 <4nz0@proton.me>
2022-08-11 16:28:51 +0200

    style: black

--- coingecko_cli/cli.py
+++ coingecko_cli/cli.py
@@ -4,10 +4,12 @@ from typer import Typer
 
 app = Typer()
 
+
 @app.command()
 def ping():
-    r = httpx.get('https://api.coingecko.com/api/v3/ping').json()['gecko_says']
+    r = httpx.get("https://api.coingecko.com/api/v3/ping").json()["gecko_says"]
     print(r)
+
 
 if __name__ == "__main__":
     app()
--- tests/test_coingecko_cli.py
+++ tests/test_coingecko_cli.py
@@ -2,4 +2,4 @@ from coingecko_cli import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
