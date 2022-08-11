7eca66b660187b22b229886431dca85083c3e0d5
0x4nz0 <4nz0@proton.me>
2022-08-11 16:36:35 +0200

    feat: trending command

--- coingecko_cli/cli.py
+++ coingecko_cli/cli.py
@@ -11,5 +11,11 @@ def ping():
     print(r)
 
 
+@app.command()
+def trending():
+    r = httpx.get("https://api.coingecko.com/api/v3/search/trending").json()["coins"]
+    print(r)
+
+
 if __name__ == "__main__":
     app()
