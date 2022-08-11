f9cfda0ee88b3b6955fea30b5c37bbd752fda1f7
0x4nz0 <4nz0@proton.me>
2022-08-11 16:13:49 +0200

    feat: ping command

---
+++ coingecko_cli/cli.py
@@ -0,0 +1,13 @@
+import httpx
+from rich import print
+from typer import Typer
+
+app = Typer()
+
+@app.command()
+def ping():
+    r = httpx.get('https://api.coingecko.com/api/v3/ping').json()['gecko_says']
+    print(r)
+
+if __name__ == "__main__":
+    app()
