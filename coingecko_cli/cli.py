52288dd079f44cb5ad8cbedada3400bf027f09fa
0x4nz0 <4nz0@proton.me>
2022-08-12 14:46:35 +0200

    refactor: add help for the commands

--- coingecko_cli/cli.py
+++ coingecko_cli/cli.py
@@ -7,12 +7,18 @@ app = Typer()
 
 @app.command()
 def ping():
+    """
+    Check API server status
+    """
     r = httpx.get("https://api.coingecko.com/api/v3/ping").json()["gecko_says"]
     print(r)
 
 
 @app.command()
 def trending():
+    """
+    Top-7 trending coins on CoinGecko as searched by users in the last 24 hours (Ordered by most popular first)
+    """
     r = httpx.get("https://api.coingecko.com/api/v3/search/trending").json()["coins"]
     print(r)
 
