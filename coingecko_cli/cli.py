19ecbccdc3b7a734be540066e973ed013584454c
0x4nz0 <4nz0@proton.me>
2022-08-13 15:12:56 +0200

    refactor: print data in a table for trending command

--- coingecko_cli/cli.py
+++ coingecko_cli/cli.py
@@ -1,6 +1,10 @@
 import httpx
-from rich import print
+from rich.console import Console
+from rich.table import Table
 from typer import Typer
+
+
+console = Console()
 
 app = Typer()
 
@@ -11,7 +15,7 @@ def ping():
     Check API server status
     """
     r = httpx.get("https://api.coingecko.com/api/v3/ping").json()["gecko_says"]
-    print(r)
+    console.print(r)
 
 
 @app.command()
@@ -20,7 +24,26 @@ def trending():
     Top-7 trending coins on CoinGecko as searched by users in the last 24 hours (Ordered by most popular first)
     """
     r = httpx.get("https://api.coingecko.com/api/v3/search/trending").json()["coins"]
-    print(r)
+    table = Table("id", "coin_id", "name", "symbol", "market_cap_rank", "price_btc")
+    for coin in r:
+        coin = coin["item"]
+        (
+            _id,
+            coin_id,
+            name,
+            symbol,
+            market_cap_rank,
+            _,
+            _,
+            _,
+            _,
+            price_btc,
+            _,
+        ) = coin.values()
+        table.add_row(
+            _id, str(coin_id), name, symbol, str(market_cap_rank), str(price_btc)
+        )
+    console.print(table)
 
 
 if __name__ == "__main__":
