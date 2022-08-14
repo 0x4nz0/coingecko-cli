21e6adde1930e5993e3ba57750dd3e381b55d8ee
0x4nz0 <4nz0@proton.me>
2022-08-14 11:33:27 +0200

    refactor: URL query parameters in the request

--- coingecko_cli/cli.py
+++ coingecko_cli/cli.py
@@ -9,13 +9,15 @@ console = Console()
 
 app = Typer()
 
+API_BASE_URL = "https://api.coingecko.com/api/v3"
+
 
 @app.command()
 def ping():
     """
     Check API server status
     """
-    r = httpx.get("https://api.coingecko.com/api/v3/ping").json()["gecko_says"]
+    r = httpx.get(f"{API_BASE_URL}/ping").json()["gecko_says"]
     console.print(r)
 
 
@@ -31,9 +33,15 @@ def price(
     """
     Get the current price of any cryptocurrencies in any other supported currencies that you need
     """
-    r = httpx.get(
-        f"https://api.coingecko.com/api/v3/simple/price?ids={ids}&vs_currencies={vs_currencies}&include_market_cap={str(include_market_cap).lower()}&include_24hr_vol={str(include_24hr_vol).lower()}&include_24hr_change={str(include_24hr_change).lower()}&include_last_updated_at={str(include_last_updated_at).lower()}"
-    ).json()
+    params = {
+        "ids": ids,
+        "vs_currencies": vs_currencies,
+        "include_market_cap": str(include_market_cap).lower(),
+        "include_24hr_vol": str(include_24hr_vol).lower(),
+        "include_24hr_change": str(include_24hr_change).lower(),
+        "include_last_updated_at": str(include_last_updated_at).lower(),
+    }
+    r = httpx.get(f"{API_BASE_URL}/simple/price", params=params).json()
     console.print(r)
 
 
@@ -42,9 +50,7 @@ def supported_vs_currencies():
     """
     List of supported_vs_currencies
     """
-    r = httpx.get(
-        "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
-    ).json()
+    r = httpx.get(f"{API_BASE_URL}/simple/supported_vs_currencies").json()
     console.print(Columns([f"- [yellow]{currency}" for currency in r]))
 
 
@@ -53,7 +59,7 @@ def trending():
     """
     Top-7 trending coins on CoinGecko as searched by users in the last 24 hours (Ordered by most popular first)
     """
-    r = httpx.get("https://api.coingecko.com/api/v3/search/trending").json()["coins"]
+    r = httpx.get(f"{API_BASE_URL}/search/trending").json()["coins"]
     table = Table("id", "coin_id", "name", "symbol", "market_cap_rank", "price_btc")
     for coin in r:
         coin = coin["item"]
