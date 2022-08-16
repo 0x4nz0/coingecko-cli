942711395986f7f91eb204052a0579c3bc6817a8
0x4nz0 <4nz0@proton.me>
2022-08-16 09:49:35 +0200

    feat: categories market_cap command

--- coingecko_cli/categories.py
+++ coingecko_cli/categories.py
@@ -2,12 +2,22 @@ import httpx
 from rich.console import Console
 from rich.table import Table
 from typer import Typer
+from enum import Enum
 
 from .utils import API_BASE_URL
 
 console = Console()
 
 app = Typer()
+
+
+class Order(str, Enum):
+    market_cap_desc = "market_cap_desc"
+    market_cap_asc = "market_cap_asc"
+    name_desc = "name_desc"
+    name_asc = "name_asc"
+    market_cap_change_24h_desc = "market_cap_change_24h_desc"
+    market_cap_change_24h_asc = "market_cap_change_24h_asc"
 
 
 @app.command()
@@ -21,6 +31,16 @@ def list():
         category_id, name = coin.values()
         table.add_row(category_id, name)
     console.print(table)
+
+
+@app.command()
+def market_data(order: Order = Order.market_cap_desc):
+    """
+    List all categories with market data
+    """
+    params = {"order": order.value}
+    r = httpx.get(f"{API_BASE_URL}/coins/categories", params=params).json()
+    console.print(r)
 
 
 if __name__ == "__main__":
