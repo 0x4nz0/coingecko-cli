0367d9e3e7b3003ae3d77014a648290bfe55f01d
0x4nz0 <4nz0@proton.me>
2022-08-17 16:48:39 +0200

    feat: contract market_chart command

--- coingecko_cli/contract.py
+++ coingecko_cli/contract.py
@@ -1,6 +1,6 @@
 import httpx
 from rich.console import Console
-from typer import Typer
+from typer import Typer, Argument
 
 from .utils import API_BASE_URL
 
@@ -15,6 +15,22 @@ def info(id: str, contract_address: str):
     Get coin info from contract address
     """
     r = httpx.get(f"{API_BASE_URL}/coins/{id}/contract/{contract_address}").json()
+    console.print(r)
+
+
+@app.command()
+def market_chart(
+    id: str, contract_address: str, vs_currency: str, days: int = Argument(..., min=1)
+):
+    """
+    Get historical market data include price, market cap, and 24h volume
+    (granularity auto)
+    """
+    params = {"vs_currency": vs_currency, "days": str(days)}
+    r = httpx.get(
+        f"{API_BASE_URL}/coins/{id}/contract/{contract_address}/market_chart/",
+        params=params,
+    ).json()
     console.print(r)
 
 
