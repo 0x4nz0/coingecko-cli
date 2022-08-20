86cdfb5b340f419f63282e8611ee55d7e18a4926
0x4nz0 <4nz0@proton.me>
2022-08-20 15:51:24 +0200

    feat: derivatives exchange_data command

--- coingecko_cli/derivatives.py
+++ coingecko_cli/derivatives.py
@@ -2,12 +2,30 @@ import httpx
 from rich.console import Console
 from typer import Typer
 from enum import Enum
+from typing import Optional
 
 from .utils import API_BASE_URL
 
 console = Console()
 
 app = Typer()
+
+
+class TickerOption(str, Enum):
+    all = "all"
+    unexpired = "unexpired"
+
+
+@app.command()
+def exchange_data(id: str, include_tickers: Optional[TickerOption] = None):
+    """
+    Show derivative exchange data
+    """
+    params = {}
+    if include_tickers is not None:
+        params["include_tickers"] = include_tickers.value
+    r = httpx.get(f"{API_BASE_URL}/derivatives/exchanges/{id}", params=params).json()
+    console.print(r)
 
 
 @app.command()
