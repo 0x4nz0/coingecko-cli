38bdb8a43dab21213d5350e92dd9a03dee54ac49
0x4nz0 <4nz0@proton.me>
2022-08-20 15:59:50 +0200

    feat: derivatives exchanges command

--- coingecko_cli/derivatives.py
+++ coingecko_cli/derivatives.py
@@ -1,6 +1,6 @@
 import httpx
 from rich.console import Console
-from typer import Typer
+from typer import Typer, Option
 from enum import Enum
 from typing import Optional
 
@@ -14,6 +14,29 @@ app = Typer()
 class TickerOption(str, Enum):
     all = "all"
     unexpired = "unexpired"
+
+
+class ExchangeOrder(str, Enum):
+    name_asc = "name_asc"
+    name_desc = "name_desc"
+    open_interest_btc_asc = "open_interest_btc_asc"
+    open_interest_btc_desc = "open_interest_btc_desc"
+    trade_volume_24h_btc_asc = "trade_volume_24h_btc_asc"
+    trade_volume_24h_btc_desc = "trade_volume_24h_btc_desc"
+
+
+@app.command()
+def exchanges(
+    order: ExchangeOrder = ExchangeOrder.name_desc,
+    per_page: int = Option(100),
+    page: int = Option(1),
+):
+    """
+    List all derivative exchanges
+    """
+    params = {"order": order.value, "per_page": str(per_page), "page": str(page)}
+    r = httpx.get(f"{API_BASE_URL}/derivatives/exchanges", params=params).json()
+    console.print(r)
 
 
 @app.command()
