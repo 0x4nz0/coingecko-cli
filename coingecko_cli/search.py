3653af0d55a487ea2c0a787ff22a3fe8553b57b2
0x4nz0 <4nz0@proton.me>
2022-08-20 16:07:46 +0200

    feat: search query command

--- coingecko_cli/search.py
+++ coingecko_cli/search.py
@@ -11,9 +11,21 @@ app = Typer()
 
 
 @app.command()
+def query(query: str):
+    """
+    Search for coins, categories and markets listed on CoinGecko ordered
+    by largest Market Cap first
+    """
+    params = {"query": query}
+    r = httpx.get(f"{API_BASE_URL}/search", params=params).json()
+    console.print(r)
+
+
+@app.command()
 def trending():
     """
-    Top-7 trending coins on CoinGecko as searched by users in the last 24 hours (Ordered by most popular first)
+    Top-7 trending coins on CoinGecko as searched by users in the last 24 hours
+    (Ordered by most popular first)
     """
     r = httpx.get(f"{API_BASE_URL}/search/trending").json()["coins"]
     table = Table("id", "coin_id", "name", "symbol", "market_cap_rank", "price_btc")
